6370e2e300a567746ef528f5c9568499c09c5b33
tkli <386538588@qq.com>
2023-06-09 18:08:35 +0800

    机器学习

--- python/numpy.py
+++ python/numpy.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+# ndim: dimension 维度
 a = np.array([2, 3, 4, 5, 6]) # (5,) 1
 print(a.shape,a.ndim)
+
 b = np.array([[2, 3, 4, 5, 6]]) # (1,5) 2
 print(b.shape,b.ndim)
+
 c = np.array([[2, 3, 4, 5, 6],[2, 3, 4, 5, 6]]) # (2, 5) 2
 print(c.shape,c.ndim)
